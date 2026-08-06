a6aa35d21628088612757b3fb749f83fa6847453
Navin Tigga <146591653+SchemedCoder@users.noreply.github.com>
2026-08-06 23:58:43 +0530

    Update ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,1 @@
-from pyspark.sql import DataFrame
 
-def load_csv(spark, path: str) -> DataFrame:
-    return (
-        spark.read
-        .option("header", "true")
-        .option("inferSchema", "true")
-        .csv(path)
-    )
